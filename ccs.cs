2baecbea47c013cc2c8d7be379370d9c308f18a9
bomingfeng <fengzhenbang@126.com>
2022-06-12 10:23:07 +0800

    Create ccs.cs

---
+++ ccs.cs
@@ -0,0 +1,147 @@
+
+#main
+{
+    width:1000px;
+    height:550px;
+    position:absolute;
+    left:200px;
+    top:10px;
+    border-radius:10px;
+    background-color:#33cc33;
+}
+
+#title
+{
+    width:1000px;
+    height:60px;
+    position:absolute;
+    left:0px;
+    top:0px;
+    border-radius:10px;
+    line-height:60px;
+    color:red;
+    background-color:#ccffcc;
+    font-size:25px;
+    font-weight:25px;
+}
+
+#head
+{
+    width:1000px;
+    height:40px;
+    position:absolute;
+    left:0px;
+    top:50px;
+    background-color:#009900;
+    border-radius:10px;
+    overflow:hidden; 
+}
+
+
+#head2
+{
+    width:1000px;
+    height:30px;
+    position:absolute;
+    left:0px;
+    top:0px;
+    background-color:#009900;
+    border-radius:10px;
+    overflow:hidden; 
+}
+
+#body
+{
+    width:400px;
+    height:300px;
+    position:absolute;
+    left:10px;
+    top:150px;
+    border-radius:10px;
+    background-color:white;
+
+}
+
+
+
+ #do tr
+{
+    height:30px;
+    line-height:30px;
+    color:blue;
+}
+
+#do td
+{
+    height:30px;
+    width:100px;
+    line-height:30px;
+
+}
+
+#DI_flag
+{
+    width:400px;
+    height:300px;
+    position:absolute;
+    left:450px;
+    top:150px;
+    border-radius:10px;
+    background-color:white;
+
+}
+
+#DI_table tr
+{
+ height:30px;
+    line-height:30px;
+    color:blue;
+}
+#DI_table td
+{
+    height:30px;
+    width:100px;
+    line-height:30px;
+}
+
+
+.goto
+{ 
+    width:100px; 
+    height:40px; 
+    float:left; 
+    text-align:center;  
+    line-height:40px;
+    background-color:#009900; 
+    border-radius:10px; 
+    border:3px solid #009900;  
+
+}
+.goto a:link {color:yellow; text-decoration: none;}  
+.goto a:hover {background-color:#33cc33;border-radius:10px;border:7px solid #33cc33;text-decoration: none;} 
+.goto a:active {color:#33cc33; text-decoration: none;} 
+.goto a:visited {color:#ffff00 ; text-decoration: none;} 
+
+.goto2
+{ 
+    width:100px; 
+    height:30px; 
+    float:left; 
+    text-align:center;  
+    line-height:30px;
+    background-color:#009900; 
+    border-radius:10px; 
+    border:3px solid #009900;  
+
+}
+.goto2 a:link {color:yellow; text-decoration: none;}  
+.goto2 a:hover {background-color:#33cc33;border-radius:10px;border:7px solid #33cc33;text-decoration: none;} 
+.goto2 a:active {color:#33cc33; text-decoration: none;} 
+.goto2 a:visited {color:#ffff00 ; text-decoration: none;} 
+.text_border
+{
+border:1px solid green;
+}
+
+
+
